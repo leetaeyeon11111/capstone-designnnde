22008be1425ccc24863125eb927e0f7e0c00464b
leetaeyeon11111 <hahaha2662@naver.com>
2026-04-19 14:52:20 +0900

    2modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import SignUpPage from './components/SignUpPage';
